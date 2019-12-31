9d4f3fd47f5c77c8e0a02dc3d4f38b516e5671e8
Jack Works <jackworks@protonmail.com>
2019-12-31 16:54:00 +0800

    feat: fix import @types/node

--- src/dtsFixes.ts
+++ src/dtsFixes.ts
@@ -1,20 +1,36 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { log } from './log'
 import { join } from 'path'
+import { Project } from 'ts-morph'
+import { SourceFileReplacer } from './SourceFileReplacer'
 
 export function dtsFixes(dtsRoot: string) {
-    change('client.d.ts', x =>
-        x.replace(
-            /prepareKeyBackupVersion\(password: string, .+\): Promise/g,
-            'prepareKeyBackupVersion(password: string, opts?: { secureSecretStorage: boolean }): Promise'
-        )
-    )
-
-    function change(_: string, cb: (str: string) => string) {
-        const r = join(dtsRoot, _)
-        log('Patching ', r)
-        const s = readFileSync(r, 'utf-8')
-        const f = cb(s)
-        writeFileSync(r, f, 'utf-8')
+    const dtsProject = new Project({
+        compilerOptions: {}
+    })
+    dtsProject.addSourceFilesAtPaths(join(dtsRoot, '**/*.d.ts'))
+    for (const each of dtsProject
+        .getSourceFiles()
+        .map(x => new SourceFileReplacer(x))) {
+        if (each.sourceFile.getFilePath().endsWith('client.d.ts')) {
+            each.replace(x =>
+                x.replace(
+                    /prepareKeyBackupVersion\(password: string, .+\): Promise/g,
+                    'prepareKeyBackupVersion(password: string, opts?: { secureSecretStorage: boolean }): Promise'
+                )
+            )
+            each.apply()
+        }
+        each.touchSourceFile(s => {
+            const i = s
+                .getImportDeclarations()
+                .filter(x =>
+                    x
+                        .getModuleSpecifierValue()
+                        .endsWith('node_modules/@types/node/events')
+                )
+            i.forEach(x => x.setModuleSpecifier('events'))
+        })
     }
+    dtsProject.saveSync()
 }
