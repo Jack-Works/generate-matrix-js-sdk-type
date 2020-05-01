3c6f0958fa150b7ef7b3659f85452899a2578ada
Jack Works <jackworks@protonmail.com>
2020-05-01 12:39:57 +0800

    fix: callback

--- src/preFix.ts
+++ src/preFix.ts
@@ -3,13 +3,13 @@ import { SourceFileReplacer } from './SourceFileReplacer'
 import { log } from './log'
 
 export function preFix(project: Project, matrixRoot: string) {
-    for (const _ of project.getSourceFiles().map(x => new SourceFileReplacer(x))) {
+    for (const _ of project.getSourceFiles().map((x) => new SourceFileReplacer(x))) {
         const path = _.sourceFile.getFilePath()
         log(`Run prefix for: ${path}`)
 
         // https://github.com/microsoft/TypeScript/pull/35219
         if (path.endsWith('store/memory.js')) {
-            _.replace(x =>
+            _.replace((x) =>
                 x
                     .replace(/(.+): function\(/g, 'MemoryStore.prototype.$1 = function(')
                     .replace(/^    \},$/gm, '}')
@@ -20,7 +20,7 @@ export function preFix(project: Project, matrixRoot: string) {
             )
         } else if (path.endsWith('http-api.js')) {
             // https://github.com/matrix-org/matrix-js-sdk/pull/1181
-            _.replace(x =>
+            _.replace((x) =>
                 x
                     .replace(
                         `export function MatrixError(errorJson) {`,
@@ -36,9 +36,11 @@ MatrixError.prototype.constructor = MatrixError;`,
                         '};'
                     )
             )
+        } else if (path.endsWith('client.js')) {
+            _.replace((x) => x.replace(`@callback module:client.callback`, '@callback callback'))
         }
 
-        _.replace(x =>
+        _.replace((x) =>
             x
                 // {Object.<string, function(new: module:modulePath.ExportPath)>}
                 // => {Record.<string, module:modulePath.ExportPath)>}
